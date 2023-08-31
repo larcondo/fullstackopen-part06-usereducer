6d11b9b46b455fabc885bde971080a10bb114040
larcondo <larcondo90@gmail.com>
2023-08-31 17:19:49 -0300

    add contentext to Display and Button components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,9 @@
-const Button = ({ dispatch, type, label }) => {
+import { useContext } from 'react'
+import CounterContext from '../CounterContext'
+
+const Button = ({ type, label }) => {
+  const [counter, dispatch] = useContext(CounterContext)
+  
   return (
     <button onClick={ () => dispatch({ type }) }>
       { label }
