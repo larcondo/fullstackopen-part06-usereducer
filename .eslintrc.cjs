aa3efd35a6cb454edba7fed1646e11f9e5b6728a
larcondo <larcondo90@gmail.com>
2023-08-31 17:38:14 -0300

    add eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,18 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', {'before': true, 'after': true}],
+    'no-console': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
